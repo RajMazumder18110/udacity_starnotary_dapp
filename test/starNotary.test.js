96f2b02926df211277fe162a297a747dc3b66449
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2022-05-12 23:30:49 +0530

    update test file

--- test/starNotary.test.js
+++ test/starNotary.test.js
@@ -10,6 +10,7 @@ contract('StarNotary', (accounts) => {
           user5 = accounts[5];
 
     before(async () => {
+        await StarNotary.new();
         contract = await StarNotary.deployed();
     })
 
@@ -35,7 +36,7 @@ contract('StarNotary', (accounts) => {
             result = await contract.symbol()
             assert.equal(
                 result, 'DSA',
-                "Collection name is not `DSA`"
+                "Collection symbol is not `DSA`"
             );
         })
     })
@@ -84,7 +85,7 @@ contract('StarNotary', (accounts) => {
 
     describe('Put for sale', () => {
         it('Should put user1 created star on sale', async () => {
-            const starPrice = web3.utils.toWei('0.0001', 'ether');
+            const starPrice = web3.utils.toWei('0.1', 'ether');
             await contract.putStarUpForSale(1, starPrice, { from: user1 });
             result = await contract.starsForSell(1);
             assert.equal(
@@ -97,8 +98,8 @@ contract('StarNotary', (accounts) => {
         let balanceOfUser1Before, balanceOfUser1After;
 
         it('Should buy star user2 from user1 and get the funds after the sale', async () => {
-            const starPrice = web3.utils.toWei('0.0001', 'ether');
-            const value = web3.utils.toWei('0.0005', 'ether');
+            const starPrice = web3.utils.toWei('0.1', 'ether');
+            const value = web3.utils.toWei('0.5', 'ether');
             balanceOfUser1Before = await web3.eth.getBalance(user1);
             await contract.buyStar(1, { from: user2, value });
             balanceOfUser1After = await web3.eth.getBalance(user1);
@@ -117,7 +118,7 @@ contract('StarNotary', (accounts) => {
         })
 
         it('Should decrease user2 balance after buying Star', async () => {
-            const starPrice = web3.utils.toWei('0.0001', 'ether');
+            const starPrice = web3.utils.toWei('0.1', 'ether');
             result = Number(balanceOfUser1After) - Number(balanceOfUser1Before);
             assert.equal(
                 result, starPrice,
@@ -136,12 +137,12 @@ contract('StarNotary', (accounts) => {
             result = await contract.ownerOf(user2Token);
             assert.equal(
                 result, user3,
-                "Current token2's owwner is not account 3"
+                "Current token2's owner is not account 3"
             )
             result = await contract.ownerOf(user3Token);
             assert.equal(
                 result, user2,
-                "Current token3's owwner is not account 2"
+                "Current token3's owner is not account 2"
             )
         })
     })
@@ -150,7 +151,7 @@ contract('StarNotary', (accounts) => {
     describe('Token transfer', () => {
         it('should transfer user4 token to user5', async () => {
             const user4Token = 4
-            await contract.transferStar( user5, 4, { from: user4 })
+            await contract.transferStar( user5, user4Token, { from: user4 })
             result = await contract.ownerOf(user4Token);
             assert.equal(
                 result, user5,
